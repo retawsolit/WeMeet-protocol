0964be71809d70191c875c103e0ae85d11cba376
retawsolit <pykepyke147@gmail.com>
2025-08-18 21:26:07 +0700

    config

---
+++ generate.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+PROTO_TMP_DIR="./tmp"
+PROTO_VALIDATOR_DIR="${PROTO_TMP_DIR}/protovalidate"
+LIVEKIT_PROTO_DIR="${PROTO_TMP_DIR}/livekit-protocol"
+
+if [ ! -d ${PROTO_TMP_DIR} ]; then
+  mkdir -p ${PROTO_TMP_DIR}
+fi
+
+if [ ! -d ${PROTO_VALIDATOR_DIR} ]; then
+  git clone https://github.com/bufbuild/protovalidate ${PROTO_VALIDATOR_DIR}
+fi
+
+if [ ! -d ${LIVEKIT_PROTO_DIR} ]; then
+  git clone https://github.com/livekit/protocol ${LIVEKIT_PROTO_DIR}
+fi
+
+
+if ! which buf >/dev/null; then
+  printf "buf not installed, installing using go install"
+  go install github.com/bufbuild/buf/cmd/buf@latest
+fi
+
+buf dep update
+buf generate
+
